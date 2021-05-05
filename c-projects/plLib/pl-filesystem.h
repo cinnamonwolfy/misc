912a6b64b460221210ef9b7c3056d29ba625ea1a
pocketlinux32 <pocketlinux32@gmail.com>
2021-05-04 23:01:33 -0500

    file stream

--- c-projects/plLib/pl-filesystem.h
+++ c-projects/plLib/pl-filesystem.h
@@ -10,14 +10,16 @@
 #include <sys/stat.h>
 #include <dirent.h>
 
+// plDir: Directory Structure
 struct plDir {
-	char* dirPath;
-	char** dirEntries;
-	unsigned int amtEntries;
+	char* dirPath; // directory path
+	char** dirEntries; // memory-allocated listing of directory
+	size_t amtEntries; // amount of entries
 }
 
 struct plFile {
 	char** buffer;
+	size_t bufferSize;
 	FILE* fileObject;
 }
 
@@ -30,8 +32,9 @@ void plDeallocDirInfo(struct plDir* dirInfo){
 struct plDir* plGetDirInfo(const char* path){
 	DIR* tempDir = opendir(path);
 	static char** list;
-	unsigned int amount = 0, i = 0;
-	struct plDir* returnStruct;
+	size_t amount = 0;
+	int i = 0;
+	struct plDir returnStruct;
 	struct dirent* tempFileStruct;
 
 	while(tempFileStruct = readdir(tempDir)){
@@ -52,7 +55,15 @@ struct plDir* plGetDirInfo(const char* path){
 	returnStruct.dirEntries = list;
 	returnStruct.amtEntries = amount;
 
-	return returnStruct;
+	return returnStruct*;
 }
 
-void
+struct plFile* plFileStream(const char* buf, size_t bufSize, bool isStringStream, const char* path){
+	FILE* tempFile;
+	struct plFile returnStruct;
+	if(isStringStream){
+		tempFile = open_memstream(buf*, bufSize*);
+	}else{
+		tempFile = fopen(path);
+	}
+}
