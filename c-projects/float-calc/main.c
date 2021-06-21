e4078530d5cb9cb6681a64b682ca4c282e32bb9a
pocketlinux32 <pocketlinux32@gmail.com>
2021-06-21 00:40:21 -0500

    improvements to float-calc

--- c-projects/float-calc/main.c
+++ c-projects/float-calc/main.c
@@ -17,12 +17,29 @@ double calculate(double nums[], char op){
 
 int main(int argc, const char* argv[]){
 	if(argc - 1 < 3){
-		printf("Error: Invocation should include 3 parameters, only %d parameters were provided\n", argc - 1);
+		printf("Error: Invocation should include at least 3 parameters, only %d parameters were provided\n", argc - 1);
+		exit(1);
+	}else if((argc - 1) % 2 != 1){
+		printf("Error: Invocation should include a parameter amount of an odd number. An even number of parameters were provided: %d\n", argc - 1);
 		exit(1);
 	}
 
-	double numbers[2] = {atof(argv[1]), atof(argv[3])};
-	double result = calculate(numbers, argv[2][0]);
+	double result = 0;
+
+	for(int i = 1; i < argc - 1; i+=2){
+		double numbers[2];
+		if(i == 1){
+			numbers[0] = atof(argv[i]);
+			numbers[1] = atof(argv[i + 2]);
+		}else{
+			numbers[0] = result;
+			numbers[1] = atof(argv[i + 2]);
+		}
+		result = calculate(numbers, argv[i + 1][0]);
+	}
+
+//	double numbers[2] = {atof(argv[1]), atof(argv[3])};
+//	double result = calculate(numbers, argv[2][0]);
 
 	printf("%f\n", result);
 	exit(0);
