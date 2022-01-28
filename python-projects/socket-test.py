c678e80b64264eaaf1b4fffa5474720fc602cd6e
pocketlinux32 <pocketlinux32@gmail.com>
2022-01-27 22:07:38 -0600

    more nonsense

---
+++ python-projects/socket-test.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+import socket
+
+def packetParser(data):
+	return 0
+
+if __name__ == "__main__":
+	socklisten = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	socklisten.bind(("localhost", 25755))
+	socklisten.listen(5)
+	print("Server initialized on port 25755")
+	while True:
+		(connection, address) = socklisten.accept()
+		data = connection.recv(1024)
+		while data:
+			print("Received data: ", repr(data))
+			data = connection.recv(1024)
