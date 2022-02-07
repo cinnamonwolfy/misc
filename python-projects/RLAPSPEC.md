144dd56d3afc884bc3280717fd9dd800a27981c8
pocketlinux32 <pocketlinux32@gmail.com>
2022-02-07 07:30:39 -0600

    nano

---
+++ python-projects/RLAPSPEC.md
@@ -0,0 +1,60 @@
+# Remote Login-Auth Protocol Rough Draft
+## Author: pocketlinux32
+
+### Introduction
+
+About a year ago, I had created a login system for my file server, so that I
+could manage it without the need of an SFTP client. I never ended up truly
+sticking with it, due to the fact that it was extremely hacky (I would have to
+start the login-auth script every time I wanted to authenticate a user. I would
+do this from the shell, and I would give it plaintext passwords, not salted
+ones). This protocol shall make it possible to authenticate login credentials
+securely
+
+### Example Packets
+
+This would be a client-side packet/request
+```
+RLAP CLIENT 0.01
+RLAP_STARTCOMM
+USER pocketlinux32 PLAINPASS password
+USER lightripp3r HASHPASS $HASH_PASS
+USER rj SSHKEY $SSH_KEY
+... (more entries)
+RLAP_ENDCOMM
+
+```
+This would be a server-side packet/response
+```
+RLAP SERVER 0.01
+RLAP_STARTCOMM
+ENTRY 1 YES USER NO PASS
+ENTRY 2 NO USER
+ENTRY 3 YES USER YES SSHKEY
+... (more entries)
+RLAP_ENDCOMM
+```
+
+### Packet Layout
+
+An RLAP packet always starts by stating it's an RLAP packet, then it states its
+role and protocol version.
+```
+RLAP CLIENT 0.01
+```
+The packet then declares the start of the data
+```
+RLAP_STARTCOMM
+```
+Then it either gives a list of users and passwords (either plaintext or salted)
+if a client, or a list of results if a server
+```
+# Client
+USER pocketlinux32 HASHPASS $HASH_PASS
+# Server
+ENTRY 1 YES USER NO PASS
+```
+Finally, at the end of the list, the end of the data stream is declared
+```
+RLAP_ENDCOMM
+```
--- python-projects/login-auth.py
+++ python-projects/login-auth.py
@@ -1,27 +1,61 @@
 #!/usr/bin/python3
 
 import socket
-rlpver = '0.01'
+
+bport = 25755
+rlapver = b'0.01'
+startcomm = False
+connection = ('', '')
+address = ('', '')
+
+def packetSender(data):
+	if 
+	connection.send(b'RLAP SERVER 0.01\n' + data)
 
 def packetParser(data):
 	datatokens = data.split();
+	global startcomm
+	global rlapver
 
-	if()
+	if type(datatokens) is not list and not startcomm:
+		print("RLAP Error 1: Invalid packet")
+		packetSend(b'E1: RLAP_ERR_INV_PKT\n')
+		return 1
+	elif type(datatokens) is list and not startcomm:
+		if datatokens[0] != b'RLAP':
+			print("RLAP Error 2: Incompatible protocol")
+			pa(b'E2: RLAP_ERR_INCOMPAT_PROT')
+			return 2
+		else:
+			if datatokens[1] != b'CLIENT' or datatokens[2] != rlapver:
+				print("RLAP Error 3: Incompatible version")
+				connection.send(b'E3: RLAP_ERR_INCOMPAT_VER')
+				return 3
+			else:
+				startcomm = True
+	else:
+		print("Data received, yet to write an actual authenticator")
 
 	print(repr(datatokens))
-
 	return 0
 
 if __name__ == "__main__":
 	data = b'begin'
+
 	socklisten = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	socklisten.bind(("localhost", 25755))
+	socklisten.bind(("localhost", bport))
 	socklisten.listen(5)
-	print("PocketRLP Server initialized on port 25755")
+	print("PocketRLAP Server, v0.01")
+	print("(c)2022 pocketlinux32, Under GPLv3")
+	print("Server initialized on port", bport)
 	while True:
 		(connection, address) = socklisten.accept()
-		print("Connection established with", address[0], "on port", address[1])
+		print("Connection established with host", address[0], "on port", address[1])
 
 		while data:
 			data = connection.recv(4096)
-			packetParser(data)
+			if packetParser(data):
+				break
+
+		print("Ended connection with host", address[0])
+		startcomm = False
