193d240016d9903bde5963e1d9ef65567443c8b2
pocketlinux32 <pocketlinux32@gmail.com>
2022-02-08 22:22:19 -0600

    nano

--- python-projects/socket-utils/socket-test.py
+++ python-projects/socket-utils/socket-test.py
@@ -10,9 +10,15 @@ if __name__ == "__main__":
 	socklisten.bind(("localhost", 25755))
 	socklisten.listen(5)
 	print("Server initialized on port 25755")
-	while True:
-		(connection, address) = socklisten.accept()
-		data = connection.recv(1024)
-		while data:
-			print("Received data: ", repr(data))
+	try:
+		while True:
+			(connection, address) = socklisten.accept()
 			data = connection.recv(1024)
+			while data:
+				print("Received data: ", repr(data))
+				data = connection.recv(1024)
+	except KeyboardInterrupt:
+		socklisten.shutdown(socket.SHUT_RDWR)
+		socklisten.close()
+		print("Server stopped")
+		exit(0)
