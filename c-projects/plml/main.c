77c75ccbca2dc03a65d78dfd5839004aafb49f38
pocketlinux32 <pocketlinux32@gmail.com>
2023-01-08 20:50:43 -0600

    okie

--- c-projects/plml/main.c
+++ c-projects/plml/main.c
@@ -5,6 +5,11 @@ typedef struct pltoken {
 	char* value;
 } pltoken_t;
 
+typedef struct pltokgroup {
+	char* name;
+	pltoken_t* ;
+}
+
 int main(int argc, const char* argv[]){
 	if(argc < 1)
 		return 1;
@@ -14,6 +19,8 @@ int main(int argc, const char* argv[]){
 	plmt_t* mt = plMTInit(0);
 	plfile_t* fileToParse = plFOpen(argv[1], "r");
 	char lineBuffer[4096];
+
+	if()
 
 	return 0;
 }
