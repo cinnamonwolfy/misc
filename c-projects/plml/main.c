fe46b44cf73cfd77b8f7e0c55c05f9310cbcdd4c
pocketlinux32 <pocketlinux32@gmail.com>
2023-01-16 22:24:17 -0600

    nano

--- c-projects/plml/main.c
+++ c-projects/plml/main.c
@@ -1,26 +1,66 @@
 #include <pl32.h>
+#define PLML_FILE 1
+#define PLML_INVALID 2
+
+#define PLML_TYPE_STRING 3
+#define PLML_TYPE_BOOL 4
+#define PLML_TYPE_INT 5
+#define PLML_TYPE_HEADER 6
 
 typedef struct pltoken {
-	char* name;
-	char* value;
+	string_t name;
+	int type;
+	string_t value;
 } pltoken_t;
 
-typedef struct pltokgroup {
-	char* name;
-	pltoken_t* ;
+void plMLError(string_t name, int errNum, plmt_t* mt){
+	printf("%s: ", name);
+
+	switch(errNum){
+		case 0:
+			printf("Success\n");
+			return;
+		case PLML_FILE:
+			printf("Couldn't open file\n");
+			break;
+		case PLML_INVALID:
+			printf("Invalid token\n");
+			break;
+		default:
+			printf("General Fatal Error\n");
+			break;
+	}
+
+	plMTStop(mt);
+	exit(errNum);
 }
 
-int main(int argc, const char* argv[]){
+pltoken_t* plMLParse(string_t string, plmt_t* mt){
+	plarray_t* tokenizedStr = plParser(lineBuffer, mt);
+	if(tokenizedStr->size != 3)
+		plMLError("plMLParse", PLML_INVALID, mt);
+
+	byte_t* basicStringStart = strchr(string, '"');
+	byte_t* literalStringStart = strchr(string, '\'');
+
+	
+}
+
+int main(int argc, string_t* argv[]){
 	if(argc < 1)
 		return 1;
 
 	printf("Parsing PLML...");
 
 	plmt_t* mt = plMTInit(0);
-	plfile_t* fileToParse = plFOpen(argv[1], "r");
+	plfile_t* fileToParse = plFOpen(argv[1], "r", mt);
 	char lineBuffer[4096];
 
-	if()
+	if(fileToParse == NULL)
+		plMLError("plFOpen", PLML_FILE);
+
+	while(plFGets(lineBuffer, 4095, fileToParse) != NULL)
+		plMLParse(lineBuffer, mt);
 
 	return 0;
 }
